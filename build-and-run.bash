9e8cad6325ddb0e7c1a976a428883e629adede15
Dave Potts <dave@goopot.co.uk>
2014-08-17 22:00:32 +0100

    Fixing build script

--- build-and-run.bash
+++ build-and-run.bash
@@ -2,6 +2,6 @@
 
 sudo docker build -t local:flask_latest flaskserver
 
-CONTAINER=$(sudo docker run -p 8888:5000 -d :flask-latest)
+CONTAINER=$(sudo docker run -p 8888:5000 -d local:flask-latest)
 
 sudo docker log $CONTAINER
