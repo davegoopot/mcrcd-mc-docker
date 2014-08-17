1f63a552c09b96f54fb77864bf1c74fa5d2ff5e1
Dave Potts <dave@goopot.co.uk>
2014-08-17 21:37:12 +0100

    Reading the file back from the web page

--- flaskserver/flask/mc-manage.py
+++ flaskserver/flask/mc-manage.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('manage.html') 
+
+@app.route('/save-file', methods=['POST'])
+def save_file():
+   print("got: " + request.form['code'])
+
 
 if __name__ == '__main__':
     app.debug = True
