ad7fc2a232b94ea1275f9ed89fbaa358413819a1
Dave Potts <dave@goopot.co.uk>
2014-09-13 09:03:25 +0000

    don't auto run in build file

--- run-and-link.bash
+++ run-and-link.bash
@@ -6,5 +6,5 @@ sudo docker rm -f mc
 sudo docker build -t local:mc_latest mcserver
 sudo docker build -t local:flask_latest flaskserver
 
-sudo docker run -d -p 25565:25565 -p 4711:4711 --name mc local:mc_latest
+sudo docker run -d -p 25565:25565 -p 4711:4711 --name mc local:mc_latest java -jar craftbukkit-beta.jar --noconsole 
 sudo docker run -d -p 8888:5000  --name web --link mc:mc local:flask_latest
