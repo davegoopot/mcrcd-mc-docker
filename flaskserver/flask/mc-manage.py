0a5077185f8bf1957cb6bf5fd1b3e55048b2265c
Dave Potts <dave@goopot.co.uk>
2014-10-05 07:16:41 +0000

    rename run to manage and update to pop open window when running script

--- flaskserver/flask/mc-manage.py
+++ flaskserver/flask/mc-manage.py
@@ -40,36 +40,64 @@ def write_script(name, code):
 
 @app.route('/')
 def root():
-    return redirect(url_for('run'))
+    return redirect(url_for('manage'))
         
-@app.route('/run')
-def run():
+@app.route('/manage')
+def manage():
     """
     Handle the management of the available scripts and running processes.
-    All the available scripts are shown.
-    The parameter showoutput=ID is optional.  If passed then the output is given
-    for the pass identifier, if present.
+    All the available scripts are shown with options to edit or create new.
+    
+    The UI flow is:
+        1. List the available scripts
+        2. User edits and existing script or creates a new one
+        3. User clicks to run a script
+        4. Running a script opens a new browser window to show the output
     """
     scripts = processrunner.list_scripts()
     scripts.sort()
-    to_run = request.args.get('script', '')
-    output = ""
-    if to_run:
-        command = "python scripts/" + to_run
-        output = processrunner.run(command, timeout=300)
     return render_template(
-        'run.html', 
-        output=output,
+        'manage.html', 
         scripts=scripts)
 
-@app.route('/output/<scriptid>')
-def showoutput(scriptid):
+@app.route('/run/<scriptname>')
+def run(scriptname):
     """
-    Return the current contents of the processrunner's output dictionary for
-    the passed scriptid.  The contents will be returned as text/plain MIME type.
+    Run the passed scriptname and show the output incrementally.
+    The page uses javascript to periodically poll for more output.  The control
+    relies on two further URIs:
+        /output/<pid>  -- returns the text/plain output for the associated pid
+        /isrunning/<pid> -- returns 'yes' or 'no' depending on whether the pid
+                            is still active or not
+    TODO:  rename run to manage and update to pop open window when running script
+    TODO:  write run method to kick off the process and return the ID
+    TODO:  write a runscript template to hold the output from the script
+    TODO:  write the output method to return the output
+    TODO:  write the isrunning method
+    TODO:  update the javascript on the runscipt template to periodically update
+           the output while the script is still running
+           the output while the script is still running
     
-    If the scriptif is not found then provide a user-friendly response, being
-    careful to sanitize the scriptid for cross-site scripting attacks.
+    
+    """
+    pass
+
+@app.route('/output/<pid>')
+def run(pid):
+    """
+    returns the text/plain output for the associated pid
+    
+    TODO
+    """
+    pass
+        
+        
+@app.route('/isrunning/<pid>')
+def run(scriptname):
+    """
+    returns 'yes' or 'no' depending on whether the pid is still active or not
+    
+    TODO
     """
     pass
         
