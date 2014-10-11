ea0793c25c2d357d1e18c2dc4b701083ed885f93
davegoopot <dave@goopot.co.uk>
2014-10-11 19:02:42 +0100

    Getting the tag right

--- push-images.bash
+++ push-images.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo docker commit --author="dave@goopot.co.uk" --message="Pushing updates for coderdojo" web davegoopot/mcrcd:web-sep2014
-sudo docker commit --author="dave@goopot.co.uk" --message="Pushing updates for coderdojo" mc davegoopot/mcrcd:mc-sep2014
+sudo docker commit --author="dave@goopot.co.uk" --message="Pushing updates for coderdojo" web davegoopot/mcrcd:web_latest
+sudo docker commit --author="dave@goopot.co.uk" --message="Pushing updates for coderdojo" mc davegoopot/mcrcd:mc_latest
 
-sudo docker push davegoopot/mcrcd:web-sep2014
-sudo docker push davegoopot/mcrcd:mc-sep2014 1
+sudo docker push davegoopot/mcrcd:web_latest
+sudo docker push davegoopot/mcrcd:mc_latest
